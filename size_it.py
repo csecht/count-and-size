e0b4ec7247949d2edb35ebf6cc32edb95fe15a86
craig <spam-me@earthlink.net>
2023-09-11 17:20:20 -0500

    v 0.0.26: move MultiProc into new parallel.py module

--- size_it.py
+++ size_it.py
@@ -36,7 +36,6 @@ Developed in Python 3.8 and 3.9, tested up to 3.11.
 
 # Standard library imports.
 import sys
-from multiprocessing.pool import Pool
 from pathlib import Path
 from statistics import mean, median
 from typing import List
@@ -45,6 +44,7 @@ from typing import List
 from utility_modules import (vcheck,
                              utils,
                              manage,
+                             parallel,
                              constants as const,
                              to_precision as to_p)
 
@@ -449,7 +449,7 @@ class ProcessImage(tk.Tk):
 
         # self.largest_ws_contours is used in select_and_size() to draw
         #   enclosing circles and calculate sizes of ws objects.
-        self.largest_ws_contours: list = MultiProc(watershed_img).pool_it
+        self.largest_ws_contours = parallel.MultiProc(watershed_img).pool_it
 
         # Convert from float32 to uint8 data type to find contours and
         #  make a PIL ImageTk.PhotoImage.
@@ -1834,65 +1834,6 @@ class ImageViewer(ProcessImage):
 
         return event
 
-
-class MultiProc:
-    """
-    A Class that handles image multiprocessing outside the tk.TK app Classes
-    so that pickling errors are avoided.
-    Methods:
-         contour_the_basins: the multiprocessing.Pool.map() func argument.
-         pool_it: handles multiprocessing.Pool.map().
-    """
-    def __init__(self, image):
-        self.image = image
-
-    def contour_the_basins(self, label: int) -> np.ndarray:
-        """
-        Used as func in multiprocess.Pool to generate watershed contours
-        for watershed basins.
-        Called from pool_it().
-        Args:
-            label: an integer element from the list of marker indexes
-                   from the labeled watershed image.
-        Returns:
-            The indexed (labeled basin) ndarray element of the largest
-            contours to be added to a contour list for object sizing.
-        """
-
-        basin_mask = np.zeros(shape=self.image.shape, dtype="uint8")
-        basin_mask[self.image == label] = 255
-
-        # Detect contours in the masked basins and return the largest one.
-        contours, _ = cv2.findContours(image=basin_mask,
-                                       mode=cv2.RETR_EXTERNAL,
-                                       method=cv2.CHAIN_APPROX_SIMPLE)
-        return max(contours, key=cv2.contourArea)
-
-    @property
-    def pool_it(self) -> list:
-        """
-        Parallel processing to find contours in the image specified by the
-        Class attribute. Calls MultiProc.contour_the_basins().
-        Called by ProcessImage.watershed_segmentation().
-        Use example: largest_contours = MP(watershed_img).pool_it
-
-        Returns:
-            List of watershed contours used for object sizing.
-         """
-        # Note: using multiprocessing to find ws contours takes less
-        #  time than finding them serially with a for-loop.
-        # DON'T use concurrent.futures or async here because we don't
-        #  want the user doing concurrent things that may wreck the flow.
-        # Note that the const.NCPU value is 1 less than then number of
-        #  physical cores.
-        # chunksize=40 was empirically optimized for speed using the
-        #  sample images on a 6-core HP Pavilion laptop.
-        with Pool(processes=const.NCPU) as mpool:
-            contours: list = mpool.map(func=self.contour_the_basins,
-                                       iterable=np.unique(ar=self.image),
-                                       chunksize=40)
-
-        return contours
 
 
 if __name__ == "__main__":
---
+++ utility_modules/parallel.py
@@ -0,0 +1,70 @@
+"""A module to run parallel processes for image processing."""
+# Standard library imports.
+
+from multiprocessing.pool import Pool
+
+import cv2
+import numpy as np
+
+# Local application imports.
+from utility_modules import constants as const
+
+
+class MultiProc:
+    """
+    A Class that handles image multiprocessing outside the tk.TK app Classes
+    so that pickling errors are avoided.
+    Methods:
+         contour_the_basins: the multiprocessing.Pool.map() func argument.
+         pool_it: handles multiprocessing.Pool.map().
+    """
+    def __init__(self, image):
+        self.image = image
+
+    def contour_the_basins(self, label: int) -> np.ndarray:
+        """
+        Used as func in multiprocess.Pool to generate watershed contours
+        for watershed basins.
+        Called from pool_it().
+        Args:
+            label: an integer element from the list of marker indexes
+                   from the labeled watershed image.
+        Returns:
+            The indexed (labeled basin) ndarray element of the largest
+            contours to be added to a contour list for object sizing.
+        """
+
+        basin_mask = np.zeros(shape=self.image.shape, dtype="uint8")
+        basin_mask[self.image == label] = 255
+
+        # Detect contours in the masked basins and return the largest one.
+        contours, _ = cv2.findContours(image=basin_mask,
+                                       mode=cv2.RETR_EXTERNAL,
+                                       method=cv2.CHAIN_APPROX_SIMPLE)
+        return max(contours, key=cv2.contourArea)
+
+    @property
+    def pool_it(self) -> list:
+        """
+        Parallel processing to find contours in the image specified by the
+        Class attribute. Calls MultiProc.contour_the_basins().
+        Called by ProcessImage.watershed_segmentation().
+        Use example: largest_contours = MP(watershed_img).pool_it
+
+        Returns:
+            List of watershed contours used for object sizing.
+         """
+        # Note: using multiprocessing to find ws contours takes less
+        #  time than finding them serially with a for-loop.
+        # DON'T use concurrent.futures or async here because we don't
+        #  want the user doing concurrent things that may wreck the flow.
+        # Note that the const.NCPU value is 1 less than then number of
+        #  physical cores.
+        # chunksize=40 was empirically optimized for speed using the
+        #  sample images on a 6-core HP Pavilion laptop.
+        with Pool(processes=const.NCPU) as mpool:
+            contours: list = mpool.map(func=self.contour_the_basins,
+                                       iterable=np.unique(ar=self.image),
+                                       chunksize=40)
+
+        return contours
